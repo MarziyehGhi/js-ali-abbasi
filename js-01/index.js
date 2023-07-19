d060000de35d7370fac3b4596bcb5b1bb1696271
MarziyehGhahramani <marziyeh.webvalue@gmail.com>
2023-07-19 17:43:09 +0330

    initial commit

---
+++ js-01/index.js
@@ -0,0 +1,12 @@
+let userAge = +prompt("Enter Your Age Namoosan!");
+console.log(userAge)
+console.log(typeof userAge)
+
+let userDOB = 2023 - userAge;
+console.log(userDOB);
+console.log(typeof userDOB)
+
+let userAfter30 = 30 + userAge;
+
+document.write("this noobi user born at ", userDOB, " year!")
+document.write("this noobi user is ", userAfter30, "si years later!")
